3d5421257c5b15e4051787124aa737e1eac2de14
Luizito <luizantunes1994@gmail.com>
2022-07-08 13:53:22 -0300

    novos exemplos do javascript

--- Module4-examples/js/script.js
+++ Module4-examples/js/script.js
@@ -1,6 +1,43 @@
-function b() {
-    return {
-        name:"Luiz"
-    };
-}
-console.log (b())
+// function b() {
+//     return {
+//         name:"Luiz"
+//     };
+// }
+// console.log (b())
+
+// var message = "in global";
+// console.log ("global: message = " + message);
+
+
+// EXEMPLO DE GLOBAL E NÃO 
+
+// var message = "in global";
+// console.log("Global: message" + message);
+
+// var a = function () {
+//     var message = "inside a";
+//     console.log("a: message =" + message);
+//     function b () {
+//         console.log("b: message =" + message);
+//     }
+//     b();
+// }
+// a ();
+
+
+//EXEMPLO DE UNDEFINED
+
+// var x;
+// console.log(x);
+
+// if (x == undefined) {
+//     console.log("x is undefined");
+// }
+
+// x = 5;
+// if (x == undefined) {
+//     console.log("x is undefined");
+// }
+// else {
+//     console.log("x has been defined");
+// }
